ea7d5be41fe7541831819facc8c08c2e3bcdf048
Tokxik <95232629+tirexellent@users.noreply.github.com>
2025-01-13 09:27:57 +0100

    Update + rename to CREATE_RDB.sql cascades still mia

--- CREATE_RDB.sql
+++ CREATE_RDB.sql
@@ -1,6 +1,8 @@
+
+
 -- CREATE RDB
 
-CREATE TABLE user_profil (
+CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     user_name VARCHAR(20) NOT NULL,
     mail VARCHAR(50) NOT NULL,
@@ -41,25 +43,26 @@ CREATE TABLE theme (
     word_limit INT NOT NULL,
     open_at  DATE DEFAULT CURRENT_TIMESTAMP,
     close_at  DATE DEFAULT CURRENT_TIMESTAMP,
+  	conditions VARCHAR(255),
     CONSTRAINT check_open_before_close CHECK (open_at <= close_at)
 );
 
 -- Création des clés étrangères après création des tables
 
 ALTER TABLE vote
-    ADD CONSTRAINT vote_user_id_fkey FOREIGN KEY (user_id) REFERENCES user_profil(user_id);
+    ADD CONSTRAINT vote_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(user_id);
 
 ALTER TABLE vote
     ADD CONSTRAINT vote_text_id_fkey FOREIGN KEY (text_id) REFERENCES text_p(text_id);
 
 ALTER TABLE comment
-    ADD CONSTRAINT comment_user_id_fkey FOREIGN KEY (user_id) REFERENCES user_profil(user_id);
+    ADD CONSTRAINT comment_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(user_id);
 
 ALTER TABLE comment
     ADD CONSTRAINT comment_text_id_fkey FOREIGN KEY (text_id) REFERENCES text_p(text_id);
 
 ALTER TABLE text_p
-    ADD CONSTRAINT text_p_user_id_fkey FOREIGN KEY (user_id) REFERENCES user_profil(user_id);
+    ADD CONSTRAINT text_p_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(user_id);
 
 ALTER TABLE text_p
     ADD CONSTRAINT text_p_theme_id_fkey FOREIGN KEY (theme_id) REFERENCES theme(theme_id);
