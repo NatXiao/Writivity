436e08cad5aea6a1c9f488212837a2c5c87111ed
NatXiao <natasha.xiao@outlook.com>
2025-01-20 10:47:13 +0100

    add profile + a little cleaning of controller

--- src/java/Controllers/BaseController.java
+++ src/java/Controllers/BaseController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
+import src.java.model.Text;
+import src.java.model.Theme;
+import src.java.model.Users;
+
+import java.sql.Array;
+import java.util.ArrayList;
 
 @Controller
 public class BaseController {
@@ -13,14 +19,22 @@ public class BaseController {
     /// Return to Home page
     @GetMapping("/")
     public String Home1() {
-        return "login";
+        return "/login";
     }
+
+    @GetMapping("/error")
+    public String Error() { return "error"; }
+
     @GetMapping("/home")
-    public String Home2() {
+    public String Home2(Model model) {
+        model.addAttribute("Theme", new ArrayList<Theme>());
         return "home";
     }
     @GetMapping("/profile")
-    public String Profile() {
+    public String Profile(Model model, Model model2,Model model3) {
+        model.addAttribute("Users", new Users());
+        model2.addAttribute("Themes", new ArrayList<Theme>());
+        model3.addAttribute("Text", new ArrayList<Text>());
         return "profile";
     }
 
--- src/java/Controllers/DemoController.java
+++ src/java/Controllers/DemoController.java
@@ -9,39 +9,9 @@ import java.time.LocalTime;
 @RestController
 public class DemoController {
 
-
     @GetMapping("/print")
     public void print() {
         System.out.println(LocalTime.now().toString());
-    }
-
-    @GetMapping("/error")
-    public String sayError() {
-        return String.format("ERROR 404 !");
-    }
-
-    @GetMapping("/redir")
-    public RedirectView redirectWithUsingRedirectView(
-            RedirectAttributes attributes) {
-        attributes.addFlashAttribute("flashAttribute", "redirectWithRedirectView");
-        attributes.addAttribute("attribute", "redirectWithRedirectView");
-
-        System.out.println("Reditection !");
-
-        return new RedirectView("/index.html");
-    }
-
-    // href="src/templates/challenge.html"
-
-    @GetMapping("/backtohome")
-    public RedirectView redirectToHome(
-            RedirectAttributes attributes) {
-        attributes.addFlashAttribute("flashAttribute", "redirectWithRedirectView");
-        attributes.addAttribute("attribute", "redirectWithRedirectView");
-
-        System.out.println("Reditection !");
-
-        return new RedirectView("/home.html");
     }
 
 }
