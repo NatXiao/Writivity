c661024dcc4bb6c67e8493339ce330f0bf42c62a
Discretos <siedeltoad@gmail.com>
2025-01-22 14:08:39 +0100

    Test Icon dans home.html

--- src/java/Controllers/BaseController.java
+++ src/java/Controllers/BaseController.java
@@ -62,7 +62,6 @@ public class BaseController {
         themes.add(t2);
         themes.add(t3);
 
-
         model.addAttribute("Theme", themes); // new ArrayList<Theme>()
         model.addAttribute("OldTheme", new ArrayList<Theme>()); // new ArrayList<Theme>()
         model.addAttribute("isAdmin", SessionManager.IsAdmin(session));
@@ -83,28 +82,7 @@ public class BaseController {
         return "createText";
     }
 
-    @GetMapping("/challengeid/{id}")
-    public String singleChallenge(@PathVariable("id") int id, Model model, HttpSession session) {
 
-        /*if (id == 1)
-            return "error403";
-
-        if(id == 7)
-            return "error";*/
-
-        Theme t4 = new Theme();
-        t4.setTheme_id(0);
-        t4.setTheme_name("Aventure");
-        t4.setWord_limit(2000);
-        t4.setOpen_at(LocalDateTime.now());
-        t4.setClose_at(LocalDateTime.of(2026, 10, 9, 12, 0, 0));
-        t4.setConditions("Sans la lettre \"Y\"");
-
-        model.addAttribute("Challenge", t4);
-
-
-        return "singleChallenge";
-    }
 
     @GetMapping("/stats")
     public String Stats(Model model, HttpSession session) {
--- src/java/Controllers/ChallengeController.java
+++ src/java/Controllers/ChallengeController.java
@@ -1,25 +1,38 @@
 package src.java.Controllers;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
-import src.java.model.LoginModel;
-import src.java.model.Text;
 import src.java.model.Theme;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 
 @Controller
 public class ChallengeController {
 
-    @GetMapping("/challengeId")
-    public String redirectToChallenge(Model model, Model model2) {
-        model.addAttribute("Challenge", new Theme());
-        model2.addAttribute("Text", new ArrayList<Text>());
+    @GetMapping("/challengeid/{id}")
+    public String singleChallenge(@PathVariable("id") int id, Model model, HttpSession session) {
+
+        /*if (id == 1)
+            return "error403";
+
+        if(id == 7)
+            return "error";*/
+
+        Theme t4 = new Theme();
+        t4.setTheme_id(0);
+        t4.setTheme_name("Aventure");
+        t4.setWord_limit(2000);
+        t4.setOpen_at(LocalDateTime.now());
+        t4.setClose_at(LocalDateTime.of(2026, 10, 9, 12, 0, 0));
+        t4.setConditions("Sans la lettre \"Y\"");
+
+        model.addAttribute("Challenge", t4);
+
 
         return "singleChallenge";
     }
@@ -34,7 +47,7 @@ public class ChallengeController {
     }
 
     @PostMapping("/register_new_challenge")
-    public String registerNewChallenge(Model model, @ModelAttribute("challenge") Theme theme){
+    public String registerNewChallenge(Model model, @ModelAttribute("challenge") Theme theme) {
 
         model.addAttribute("challenge", theme);
 
