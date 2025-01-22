313d200943640af46cefeb5cb7f8e4aca4752a62
Discretos <siedeltoad@gmail.com>
2025-01-22 12:15:13 +0100

    Lien entre /home et /singleChallenge + Test (sans DB) Ajout d'un lien vers la page stats depuis home (si admin) Correction dans error403.html

--- src/java/Controllers/BaseController.java
+++ src/java/Controllers/BaseController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import src.java.SessionManager;
 import src.java.model.Text;
 import src.java.model.Theme;
@@ -48,13 +49,23 @@ public class BaseController {
         t2.setClose_at(LocalDateTime.of(2025, 10, 9, 12, 0, 0));
         t2.setConditions("R.D.T.");
 
+        Theme t3 = new Theme();
+        t3.setTheme_id(2);
+        t3.setTheme_name("Action");
+        t3.setWord_limit(12345);
+        t3.setOpen_at(LocalDateTime.now());
+        t3.setClose_at(LocalDateTime.of(2028, 10, 9, 12, 0, 0));
+        t3.setConditions("...");
+
         List<Theme> themes = new ArrayList<>();
         themes.add(t1);
         themes.add(t2);
+        themes.add(t3);
 
 
         model.addAttribute("Theme", themes); // new ArrayList<Theme>()
         model.addAttribute("OldTheme", new ArrayList<Theme>()); // new ArrayList<Theme>()
+        model.addAttribute("isAdmin", SessionManager.IsAdmin(session));
         return "home";
     }
     @GetMapping("/profile")
@@ -71,8 +82,27 @@ public class BaseController {
     public String createText(){
         return "createText";
     }
-    @GetMapping("/challengeid")
-    public String singleChallenge(){
+
+    @GetMapping("/challengeid/{id}")
+    public String singleChallenge(@PathVariable("id") int id, Model model, HttpSession session) {
+
+        /*if (id == 1)
+            return "error403";
+
+        if(id == 7)
+            return "error";*/
+
+        Theme t4 = new Theme();
+        t4.setTheme_id(0);
+        t4.setTheme_name("Aventure");
+        t4.setWord_limit(2000);
+        t4.setOpen_at(LocalDateTime.now());
+        t4.setClose_at(LocalDateTime.of(2026, 10, 9, 12, 0, 0));
+        t4.setConditions("Sans la lettre \"Y\"");
+
+        model.addAttribute("Challenge", t4);
+
+
         return "singleChallenge";
     }
 
