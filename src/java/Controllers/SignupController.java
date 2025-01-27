66dbdcea4a8301e0916684db70354ae2013f5b7e
Discretos <siedeltoad@gmail.com>
2025-01-27 18:07:19 +0100

    L'user est automatiquement connecté après s'être inscrit !

--- src/java/Controllers/SignupController.java
+++ src/java/Controllers/SignupController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import src.java.SessionManager;
 import src.java.Utils.PasswordUtil;
 import src.java.Utils.UserRepository;
 import src.java.model.HelloObject;
@@ -88,6 +89,8 @@ public class SignupController {
         userRepository.save(user);
 
         session.setAttribute("error", "");
+
+        SessionManager.LogUser(session, user);
 
         return "redirect:/home";
     }
