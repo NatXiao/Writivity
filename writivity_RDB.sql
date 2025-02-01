d6d36bada2240214aef122a5c0f3f44c72eb5382
Discretos <152215212+Discretos2022@users.noreply.github.com>
2025-02-01 17:56:03 +0100

    Update writivity_RDB.sql
    Création de la table rating

--- writivity_RDB.sql
+++ writivity_RDB.sql
@@ -2,49 +2,49 @@
 -- CREATE RDB
 
 CREATE TABLE users (
-                       user_id SERIAL PRIMARY KEY,
-                       username VARCHAR(20) UNIQUE NOT NULL,
-                       pseudo VARCHAR(20) NOT NULL,
-                       mail VARCHAR(50) UNIQUE NOT NULL,
-                       password VARCHAR(255) NOT NULL, -- bcrypt POUR HASH LE MOTS DE PASSE
-                       isAdmin BOOL DEFAULT FALSE
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(20) UNIQUE NOT NULL,
+    pseudo VARCHAR(20) NOT NULL,
+    mail VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL, -- bcrypt POUR HASH LE MOTS DE PASSE
+    isAdmin BOOL DEFAULT FALSE
 );
 
-CREATE TABLE vote (
-                      vote_id SERIAL PRIMARY KEY,
-                      user_id INT NOT NULL,
-                      text_id INT,
-                      rating INT NOT NULL
+CREATE TABLE rating (
+    rate_id SERIAL PRIMARY KEY,
+    text_id INTEGER REFERENCES text_p(text_id),
+    user_id INTEGER REFERENCES users(user_id),
+    rate INTEGER CHECK(rate > 0 AND rate <= 5)
 );
 
 CREATE TABLE comment (
-                         comment_id SERIAL PRIMARY KEY,
-                         user_id INT NOT NULL,
-                         text_id INT NOT NULL,
-                         body VARCHAR(255) NOT NULL,
-                         reported BOOLEAN DEFAULT FALSE
+    comment_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    text_id INT NOT NULL,
+    body VARCHAR(255) NOT NULL,
+    reported BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE text_p (
-                        text_id SERIAL PRIMARY KEY,
-                        user_id INT NOT NULL,
-                        challenge_id INT NOT NULL,
-                        body TEXT NOT NULL,
-                        status VARCHAR(30),
-                        text_submit BOOLEAN,
-                        submitted_at TIMESTAMP,
-                        reported BOOLEAN DEFAULT FALSE,
-                        disqualified BOOLEAN DEFAULT FALSE
+    text_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    challenge_id INT NOT NULL,
+    body TEXT NOT NULL,
+    status VARCHAR(30),
+    text_submit BOOLEAN,
+    submitted_at TIMESTAMP,
+    reported BOOLEAN DEFAULT FALSE,
+    disqualified BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE challenge (
-                           challenge_id SERIAL PRIMARY KEY,
-                           challenge_name VARCHAR(30),
-                           word_limit INT NOT NULL,
-                           open_at  DATE DEFAULT CURRENT_TIMESTAMP,
-                           close_at  DATE DEFAULT CURRENT_TIMESTAMP,
-                           conditions VARCHAR(255),
-                           CONSTRAINT check_open_before_close CHECK (open_at <= close_at)
+    challenge_id SERIAL PRIMARY KEY,
+    challenge_name VARCHAR(30),
+    word_limit INT NOT NULL,
+    open_at  DATE DEFAULT CURRENT_TIMESTAMP,
+    close_at  DATE DEFAULT CURRENT_TIMESTAMP,
+    conditions VARCHAR(255),
+    CONSTRAINT check_open_before_close CHECK (open_at <= close_at)
 );
 
 -- Création des clés étrangères après création des tables
